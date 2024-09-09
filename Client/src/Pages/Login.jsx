2c57adcd205fed1abe2b433a1f0efa2bbba703af
Abhay Kevat <143994608+Abhaykevat23@users.noreply.github.com>
2024-09-09 23:17:06 +0530

    Update Login.jsx

--- Client/src/Pages/Login.jsx
+++ Client/src/Pages/Login.jsx
@@ -8,7 +8,7 @@ function Login() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch("data-graphicss-api.vercel.app/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
